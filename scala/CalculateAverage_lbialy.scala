cabde31ef657a4033f1411dcd4595b23ecca4344
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-01-15 13:11:22 +0100

    parallel, SWAR parsing, proper benchmarks (10 runs, drop max & min, take average) on PC with AMD Ryzen

--- scala/CalculateAverage_lbialy.scala
+++ scala/CalculateAverage_lbialy.scala
@@ -5,49 +5,171 @@ import scala.util.Using
 import java.util.TreeMap
 import java.util.HashMap
 import scala.io.Source
+import java.nio.ByteBuffer
+import java.io.RandomAccessFile
+import scala.concurrent.ExecutionContext
+import java.util.concurrent.Executors
+import java.io.File
+import scala.concurrent.*, duration.*
+import java.nio.channels.FileChannel
+import java.nio.file.Paths
 
-val file = "measurements.txt"
+val path = Paths.get("measurements.txt")
 
-case class Measurement(station: String, value: Double)
-object Measurement:
-  def apply(parts: Array[String]) = new Measurement(parts(0), parts(1).toDouble)
-
-case class ResultRow(min: Double, mean: Double, max: Double):
-  private def round(value: Double): Double = math.round(value * 10.0) / 10.0
-  override def toString(): String          = s"${round(min)}/${round(mean)}/${round(max)}"
-
-class MeasurementAggregator(
+class Result(
   var min: Double = Double.MaxValue,
   var max: Double = Double.MinValue,
   var sum: Double = 0.0,
   var count: Long = 0L
 ):
-  def toResult: ResultRow = ResultRow(min, sum / count, max)
+  def merge(other: Result): Result =
+    min = math.min(min, other.min)
+    max = math.max(max, other.max)
+    sum += other.sum
+    count += other.count
+    this
+
+  def update(measurement: Double): Result =
+    this.min = math.min(this.min, measurement)
+    this.max = math.max(this.max, measurement)
+    this.sum += measurement
+    this.count += 1
+    this
+
+  override def toString(): String = s"${min}/${sum / count}/${max}"
+
+object HashMapOps:
+  def merge(map: HashMap[String, Result], other: HashMap[String, Result]): HashMap[String, Result] =
+    other.forEach { (station, result) =>
+      val existing = map.get(station)
+      if existing == null then map.put(station, result)
+      else existing.merge(result)
+    }
+    map
+
+object Parsing:
+
+  def parseWholeBuffer(bb: ByteBuffer): HashMap[String, Result] =
+    val max = bb.capacity()
+    val map = HashMap[String, Result]()
+    var idx = 0
+    while idx + 1 < max do
+      val station = parseStation(bb, idx)
+      idx = bb.position()
+
+      val temp = parseTemperature(bb, idx) / 10.0
+      idx = bb.position()
+
+      idx += 1 // skip newline
+
+      var result = map.get(station)
+      if result == null then
+        result = Result()
+        map.put(station, result)
+      result.update(temp)
+
+    map
+
+  def findSegments(raf: RandomAccessFile, fileSize: Long, segmentCount: Int): Array[Long] =
+    val segmentSize = fileSize / segmentCount
+    val segments    = Array.ofDim[Long](segmentCount + 1)
+    var i           = 1
+    segments(0) = 0
+    while i < segmentCount + 1 do
+      segments(i) = findNextNewLine(raf, i * segmentSize)
+      i += 1
+    segments
+
+  inline def findNextNewLine(raf: RandomAccessFile, start: Long): Long =
+    var i = start
+    raf.seek(i)
+    while i < raf.length() && raf.readByte() != '\n' do i += 1
+    raf.getFilePointer()
+
+  def parseStation(bb: ByteBuffer, start: Int): String =
+    var idx = start
+    while bb.get(idx) != ';' do idx += 1
+    val arr = Array.ofDim[Byte](idx - start)
+    bb.get(start, arr)
+    val station = new String(arr)
+    bb.position(idx + 1)
+    station
+
+  def parseTemperature(bb: ByteBuffer, start: Int): Int =
+    var idx    = start
+    val first4 = bb.getInt(idx)
+    idx += 3
+
+    val b1 = ((first4 >> 24) & 0xff).toByte
+    val b2 = ((first4 >> 16) & 0xff).toByte
+    val b3 = ((first4 >> 8) & 0xff).toByte
+
+    var result = 0
+
+    if b1 == '-' then
+      if b3 == '.' then // handles: -1.0
+        idx += 1
+        result -= 10 * (b2 - '0') + first4.toByte - '0'
+      else // handles: -10.1
+        idx += 1
+        result -= 100 * (b2 - '0') + 10 * (b3 - '0') + bb.get(idx) - '0'
+        idx += 1
+    else if b2 == '.' then result += 10 * (b1 - '0') + b3 - '0' // handles: 1.0
+    else // handles: 10.1
+      idx += 1
+      result += 100 * (b1 - '0') + 10 * (b2 - '0') + first4.toByte - '0'
+
+    bb.position(idx)
+
+    result
+  end parseTemperature
 
 /** Changelog:
-  *
-  * Baseline - Java: 211200ms (Oracle GraalVM 21.0.1)
-  * Baseline - Scala JVM: 209240ms (Oracle GraalVM 21.0.1)
-  * Baseline - Scala Native: 194560ms 
+  * JVM: Oracle GraalVM 21.0.1
+  * Machine: AMD Ryzen 7 2700X Eight-Core @ 16x 3.7GHz
+  * 
+  * Notes:
+  * - inlining parseStation and parseTemperature does not improve performance, on the contrary,
+  *   most java implementations manually inline these methods, maybe that's a mistake?  
+  * 
+  * baseline - Java: 194.686s 
+  * baseline - Scala JVM: 198.580s
+  * baseline - Scala Native: 342.085s (bug on Ryzen?, 197s on Apple M1 Pro)
+  * parallelism and unrolled Double parsing - Scala JVM: 7.667s
+  * faster Double parsing - Scala JVM: 7.410s
+  * 
   */
-@main def calculateAverage(): Unit = Using(Source.fromFile(file)) { source =>
-  val measurements = source.getLines().map(line => Measurement(line.split(";")))
+@main def calculateAverage(): Unit =
+  val segmentCount = Runtime.getRuntime().availableProcessors()
 
-  val toAggregate = measurements.foldLeft(HashMap[String, MeasurementAggregator]()) { (map, measurement) =>
-    val aggregator = map.computeIfAbsent(measurement.station, _ => MeasurementAggregator())
-    aggregator.min = math.min(aggregator.min, measurement.value)
-    aggregator.max = math.max(aggregator.max, measurement.value)
-    aggregator.sum += measurement.value
-    aggregator.count += 1
-    map
+  Using.Manager { use =>
+    val executor           = use(Executors.newFixedThreadPool(segmentCount))
+    given ExecutionContext = ExecutionContext.fromExecutor(executor)
+    val raf                = use(RandomAccessFile(path.toFile, "r"))
+    val fileSize           = raf.length()
+    val channel            = use(raf.getChannel())
+
+    val segments = Parsing.findSegments(raf, fileSize, segmentCount)
+
+    val maps = Future.sequence {
+      segments
+        .sliding(2)
+        .map { case Array(start, end) =>
+          Future {
+            val bb = channel.map(FileChannel.MapMode.READ_ONLY, start, end - start)
+            Parsing.parseWholeBuffer(bb)
+          }
+        }
+        .toVector
+    }
+
+    val map = Await.result(maps, Duration.Inf).reduce(HashMapOps.merge(_, _))
+
+    val finalMap = TreeMap[String, Result]()
+
+    map.forEach { (station, result) =>
+      finalMap.put(station, result)
+    }
+
+    println(finalMap)
   }
-
-  val finalMap = TreeMap[String, ResultRow]()
-
-  toAggregate.forEach { (station, aggregator) =>
-    finalMap.put(station, aggregator.toResult)
-  }
-
-  println(finalMap)
-
-}.get
---
+++ scala/CalculateAverage_lbialy.test.scala
@@ -0,0 +1,251 @@
+//> using scala 3.3.1
+//> using jvm graalvm-java21:21.0.1
+//> using toolkit default
+
+import java.nio.*
+import java.nio.file.*
+import java.io.RandomAccessFile
+import scala.util.Using
+import java.util.HashMap
+import scala.jdk.CollectionConverters.*
+import scala.concurrent.*
+import scala.concurrent.*, duration.Duration
+import java.util.concurrent.Executors
+import java.nio.channels.FileChannel
+import scala.collection.mutable
+
+class TestSuite extends munit.FunSuite:
+
+  val smallSample = """Cracow;5.1
+                      |Blantyre;37.9
+                      |Tromsø;10.9
+                      |""".stripMargin.getBytes()
+
+  val sample = """Cracow;5.1
+                 |Blantyre;37.9
+                 |Tromsø;10.9
+                 |Brisbane;20.0
+                 |Jos;33.0
+                 |Stockholm;16.7
+                 |Hobart;11.3
+                 |Villahermosa;40.7
+                 |Tabora;29.8
+                 |Niamey;21.0
+                 |Bujumbura;36.9
+                 |Muscat;40.7
+                 |Malé;29.0
+                 |Suva;19.3
+                 |Dushanbe;17.8
+                 |Abéché;42.1
+                 |Kumasi;35.3
+                 |Malé;39.7
+                 |Warsaw;-0.9
+                 |Anchorage;7.3
+                 |Kankan;34.8
+                 |Gaborone;11.2
+                 |Miami;37.1
+                 |Hanga Roa;29.2
+                 |Harbin;9.5
+                 |Mumbai;26.7
+                 |Vancouver;15.9
+                 |Barcelona;29.9
+                 |Napoli;12.5
+                 |Warsaw;14.2
+                 |Seville;12.1
+                 |Roseau;21.5
+                 |San Diego;18.0
+                 |Sapporo;15.4
+                 |""".stripMargin.getBytes()
+
+  def makeSmallSampleBB = ByteBuffer.wrap(smallSample)
+  def makeSampleBB      = ByteBuffer.wrap(sample)
+
+  def writeTempFile: Path = Files.write(Files.createTempFile("test", "txt"), sample)
+
+  test("test doubles parsing") {
+    val sample1 = ByteBuffer.wrap("1.0\n".getBytes())
+    val sample2 = ByteBuffer.wrap("10.1\n".getBytes())
+    val sample3 = ByteBuffer.wrap("99.9\n".getBytes())
+    val sample4 = ByteBuffer.wrap("-1.0\n".getBytes())
+    val sample5 = ByteBuffer.wrap("-10.1\n".getBytes())
+    val sample6 = ByteBuffer.wrap("-99.9\n".getBytes())
+    val sample7 = ByteBuffer.wrap("0.0\n".getBytes())
+    val sample8 = ByteBuffer.wrap("-0.0\n".getBytes())
+
+    assertEquals(Parsing.parseTemperature(sample1, 0) / 10.0, 1.0)
+    assertEquals(Parsing.parseTemperature(sample2, 0) / 10.0, 10.1)
+    assertEquals(Parsing.parseTemperature(sample3, 0) / 10.0, 99.9)
+    assertEquals(Parsing.parseTemperature(sample4, 0) / 10.0, -1.0)
+    assertEquals(Parsing.parseTemperature(sample5, 0) / 10.0, -10.1)
+    assertEquals(Parsing.parseTemperature(sample6, 0) / 10.0, -99.9)
+    assertEquals(Parsing.parseTemperature(sample7, 0) / 10.0, 0.0)
+    assertEquals(Parsing.parseTemperature(sample8, 0) / 10.0, -0.0)
+
+    assertEquals(sample1.get().toChar, '\n')
+    assertEquals(sample2.get().toChar, '\n')
+    assertEquals(sample3.get().toChar, '\n')
+    assertEquals(sample4.get().toChar, '\n')
+    assertEquals(sample5.get().toChar, '\n')
+    assertEquals(sample6.get().toChar, '\n')
+    assertEquals(sample7.get().toChar, '\n')
+    assertEquals(sample8.get().toChar, '\n')
+  }
+
+  test("test faster doubles parsing") {
+    val sample1 = ByteBuffer.wrap("1.0\n".getBytes())
+    val sample2 = ByteBuffer.wrap("10.1\n".getBytes())
+    val sample3 = ByteBuffer.wrap("99.9\n".getBytes())
+    val sample4 = ByteBuffer.wrap("-1.0\n".getBytes())
+    val sample5 = ByteBuffer.wrap("-10.1\n".getBytes())
+    val sample6 = ByteBuffer.wrap("-99.9\n".getBytes())
+    val sample7 = ByteBuffer.wrap("0.0\n".getBytes())
+    val sample8 = ByteBuffer.wrap("-0.0\n".getBytes())
+
+    assertEquals(Parsing.parseTemperature(sample1, 0) / 10.0, 1.0)
+    assertEquals(Parsing.parseTemperature(sample2, 0) / 10.0, 10.1)
+    assertEquals(Parsing.parseTemperature(sample3, 0) / 10.0, 99.9)
+    assertEquals(Parsing.parseTemperature(sample4, 0) / 10.0, -1.0)
+    assertEquals(Parsing.parseTemperature(sample5, 0) / 10.0, -10.1)
+    assertEquals(Parsing.parseTemperature(sample6, 0) / 10.0, -99.9)
+    assertEquals(Parsing.parseTemperature(sample7, 0) / 10.0, 0.0)
+    assertEquals(Parsing.parseTemperature(sample8, 0) / 10.0, -0.0)
+
+    assertEquals(sample1.get().toChar, '\n')
+    assertEquals(sample2.get().toChar, '\n')
+    assertEquals(sample3.get().toChar, '\n')
+    assertEquals(sample4.get().toChar, '\n')
+    assertEquals(sample5.get().toChar, '\n')
+    assertEquals(sample6.get().toChar, '\n')
+    assertEquals(sample7.get().toChar, '\n')
+    assertEquals(sample8.get().toChar, '\n')
+  }
+
+  test("linear parsing") {
+    val bb  = makeSmallSampleBB
+    val map = Parsing.parseWholeBuffer(bb).asScala
+
+    assert(map.contains("Cracow"))
+    assert(map.contains("Blantyre"))
+    assert(map.contains("Tromsø"))
+
+    assertEquals(map("Cracow").sum, 5.1)
+    assertEquals(map("Blantyre").sum, 37.9)
+    assertEquals(map("Tromsø").sum, 10.9)
+  }
+
+  test("large linear parsing") {
+    val bb  = makeSampleBB
+    val map = Parsing.parseWholeBuffer(bb).asScala
+
+    assertCorrectResult(map)
+  }
+
+  test("find segments") {
+    val segmentCount = 8
+    val path         = writeTempFile
+    val raf          = RandomAccessFile(path.toFile, "r")
+    val fileSize     = raf.length()
+
+    val segments = Parsing.findSegments(raf, fileSize, 8)
+
+    assertEquals(segments.length, segmentCount + 1)
+    assertEquals(segments.toVector, Vector(0L, 61, 118, 168, 218, 281, 330, 384, 437))
+  }
+
+  test("parallel parsing from file") {
+    val segmentCount = 6
+    val path         = writeTempFile
+
+    Using.Manager { use =>
+      val executor           = use(Executors.newFixedThreadPool(segmentCount))
+      given ExecutionContext = ExecutionContext.fromExecutor(executor)
+      val raf                = use(RandomAccessFile(path.toFile, "r"))
+      val fileSize           = raf.length()
+      val channel            = use(raf.getChannel())
+
+      val segments = Parsing.findSegments(raf, fileSize, segmentCount)
+
+      val maps = Future.sequence {
+        segments
+          .sliding(2)
+          .map { case Array(start, end) =>
+            Future {
+              val bb = channel.map(FileChannel.MapMode.READ_ONLY, start, end - start)
+              Parsing.parseWholeBuffer(bb)
+            }
+          }
+          .toVector
+      }
+
+      val map = Await.result(maps, Duration.Inf).reduce(HashMapOps.merge(_, _)).asScala
+
+      assertCorrectResult(map)
+    }
+  }
+
+  def assertCorrectResult(map: mutable.Map[String, Result]): Unit =
+    assert(map.contains("Cracow"))
+    assert(map.contains("Blantyre"))
+    assert(map.contains("Tromsø"))
+    assert(map.contains("Brisbane"))
+    assert(map.contains("Jos"))
+    assert(map.contains("Stockholm"))
+    assert(map.contains("Hobart"))
+    assert(map.contains("Villahermosa"))
+    assert(map.contains("Tabora"))
+    assert(map.contains("Niamey"))
+    assert(map.contains("Bujumbura"))
+    assert(map.contains("Muscat"))
+    assert(map.contains("Malé"))
+    assert(map.contains("Suva"))
+    assert(map.contains("Dushanbe"))
+    assert(map.contains("Abéché"))
+    assert(map.contains("Kumasi"))
+    assert(map.contains("Warsaw"))
+    assert(map.contains("Anchorage"))
+    assert(map.contains("Kankan"))
+    assert(map.contains("Gaborone"))
+    assert(map.contains("Miami"))
+    assert(map.contains("Hanga Roa"))
+    assert(map.contains("Harbin"))
+    assert(map.contains("Mumbai"))
+    assert(map.contains("Vancouver"))
+    assert(map.contains("Barcelona"))
+    assert(map.contains("Napoli"))
+    assert(map.contains("Seville"))
+    assert(map.contains("Roseau"))
+    assert(map.contains("San Diego"))
+    assert(map.contains("Sapporo"))
+
+    assertEquals(map("Cracow").sum, 5.1)
+    assertEquals(map("Blantyre").sum, 37.9)
+    assertEquals(map("Tromsø").sum, 10.9)
+    assertEquals(map("Brisbane").sum, 20.0)
+    assertEquals(map("Jos").sum, 33.0)
+    assertEquals(map("Stockholm").sum, 16.7)
+    assertEquals(map("Hobart").sum, 11.3)
+    assertEquals(map("Villahermosa").sum, 40.7)
+    assertEquals(map("Tabora").sum, 29.8)
+    assertEquals(map("Niamey").sum, 21.0)
+    assertEquals(map("Bujumbura").sum, 36.9)
+    assertEquals(map("Muscat").sum, 40.7)
+    assertEquals(map("Malé").sum, 68.7)
+    assertEquals(map("Suva").sum, 19.3)
+    assertEquals(map("Dushanbe").sum, 17.8)
+    assertEquals(map("Abéché").sum, 42.1)
+    assertEquals(map("Kumasi").sum, 35.3)
+    assertEquals(map("Warsaw").sum, 13.299999999999999)
+    assertEquals(map("Anchorage").sum, 7.3)
+    assertEquals(map("Kankan").sum, 34.8)
+    assertEquals(map("Gaborone").sum, 11.2)
+    assertEquals(map("Miami").sum, 37.1)
+    assertEquals(map("Hanga Roa").sum, 29.2)
+    assertEquals(map("Harbin").sum, 9.5)
+    assertEquals(map("Mumbai").sum, 26.7)
+    assertEquals(map("Vancouver").sum, 15.9)
+    assertEquals(map("Barcelona").sum, 29.9)
+    assertEquals(map("Napoli").sum, 12.5)
+    assertEquals(map("Seville").sum, 12.1)
+    assertEquals(map("Roseau").sum, 21.5)
+    assertEquals(map("San Diego").sum, 18.0)
+    assertEquals(map("Sapporo").sum, 15.4)
